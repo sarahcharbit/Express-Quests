f5c5760909c08cf8a1f55a059bd4055d83562c88
sarahcharbit <sarahcharbit000@gmail.com>
2023-01-31 11:22:40 +0100

    good

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = 5001;
 
 const welcome = (req, res) => {
@@ -18,7 +20,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUser);
 app.get("/api/users/:id", movieHandlers.getUserById);
-
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", movieHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
