0d9e05a141de3822cc92444dc682108681c0fb7c
sarahcharbit <sarahcharbit000@gmail.com>
2023-02-06 10:14:05 +0100

    ok

--- app.js
+++ app.js
@@ -15,15 +15,17 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const validateMovie = require ("./validators");
+const validateUser = require ("./validators");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", movieHandlers.getUser);
 app.get("/api/users/:id", movieHandlers.getUserById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", movieHandlers.postUser);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", movieHandlers.updateUser);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieHandlers.postUser);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
